28a97336241d02c74abbeb479f601a2ad20880e3
xNombre <kartapolska@gmail.com>
2017-12-01 22:20:17 +0100

    Make it soda
    It's tree taken from Upstream so we need to make some changes.

--- init/version.c
+++ init/version.c
@@ -46,5 +46,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" "Soda-Upstream-" "v1.3" ")"
+	" (" "Soda-Kernel-" "v1.3" ")"
 	" (" LINUX_COMPILER ") %s\n";
