7f7222d54d24d4c6865c95cc630a47419c6f635b
xNombre <kartapolska@gmail.com>
2018-03-19 22:25:19 +0100

    ramdisk: clean & include soda tweaks
    Now recommended tweaks will be applied on boot.

--- package/anykernel.sh
+++ package/anykernel.sh
@@ -38,6 +38,49 @@ dump_boot;
 
 # begin ramdisk changes
 
+#### init ####
+ui_print "Backup and clean ramdisks..."
+backup_file /vendor/etc/init/hw/init.qcom.rc;
+backup_file /vendor/etc/init/hw/init.qcom.power.rc;
+backup_file /vendor/etc/fstab.qcom;
+remove_line /vendor/etc/init/hw/init.qcom.rc "import soda.tweaks.rc"
+remove_line /vendor/etc/init/hw/init.qcom.power.rc "trigger tweak-soda"
+
+ui_print "Patch ramdisks..."
+# Own script
+  insert_line /vendor/etc/init/hw/init.qcom.rc "" after "import" "import soda.tweaks.rc"
+  insert_line /vendor/etc/init/hw/init.qcom.power.rc "" after "    trigger enable-low-power" "    trigger tweak-soda"
+
+# Remove default qcom power tweaks
+  remove_line /vendor/etc/init/hw/init.qcom.power.rc "setprop sys.io.scheduler"
+  remove_line /vendor/etc/init/hw/init.qcom.power.rc "write /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk"
+  remove_line /vendor/etc/init/hw/init.qcom.power.rc "write /sys/module/cpu_boost/parameters/input_boost_ms"
+  remove_line /vendor/etc/init/hw/init.qcom.power.rc "write /sys/module/cpu_boost/parameters/input_boost_freq"
+
+# Remove default io tweaks
+  remove_line /vendor/etc/init/hw/init.qcom.rc ""
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/sda/queue/iostats 0"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/sda/queue/scheduler cfq"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/sda/queue/iosched/slice_idle 0"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/sda/queue/read_ahead_kb 2048"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/sda/queue/nr_requests 256"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/dm-0/queue/read_ahead_kb 2048"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/dm-1/queue/read_ahead_kb 2048"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/sda/queue/iostats 1"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/sda/queue/nr_requests 128"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/sda/queue/read_ahead_kb 128"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/dm-0/queue/read_ahead_kb 128"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/dm-1/queue/read_ahead_kb 128"
+
+# Remove zram
+  remove_line /vendor/etc/fstab.qcom "/dev/block/zram0"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "write /sys/block/zram0/comp_algorithm"
+  remove_line /vendor/etc/init/hw/init.qcom.rc "swapon_all /vendor/etc/fstab.qcom"
+
+# Tweak flags
+  patch_fstab /vendor/etc/fstab.qcom /data ext4 options "nosuid,nodev,noatime,barrier=1,noauto_da_alloc" "nosuid,nodev,noatime,barrier=1,noauto_da_alloc,nodiratime,discard"
+  patch_fstab /vendor/etc/fstab.qcom /data f2fs options "nosuid,nodev,noatime,data_flush" "nosuid,nodev,noatime,data_flush,nodiratime,discard"
+
 # end ramdisk changes
 
 write_boot;
